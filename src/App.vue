7487b02f513617b3e60b79f430e5ed7b5f603f0a
kawadumax <kazmax7@gmail.com>
2021-06-23 20:19:43 +0900

    レモネの中央表示に成功

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container mx-auto">
+    <Top />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Top from './components/Top.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Top
+  }
+})
+</script>
+
+<style>
+</style>
---
+++ src/components/Top.vue
@@ -0,0 +1,19 @@
+<template>
+  <img src="/@assets/face.jpg" class="mx-auto" />
+</template>
+
+<script lang="ts">
+import { defineComponent, ImgHTMLAttributes, onMounted } from "vue";
+// import imgUrl from 'src/assets/face.jpg'
+export default defineComponent({
+  name: "Top",
+  setup() {
+    // onMounted(() => {
+    //   document.getElementById('face').src = imgUrl
+    // })
+  }
+
+});
+</script>
+
+<style scoped></style>
